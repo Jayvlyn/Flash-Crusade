858be443c0569bc9ecf547ce5c0c5a3e14fe9097
Jacob Leonardo <leonardojacob257@gmail.com>
2026-03-06 19:29:55 -0800

    Streaming Assets

--- Assets/_ChromaCrusade/Scripts/Shared/IO/Paths.cs
+++ Assets/_ChromaCrusade/Scripts/Shared/IO/Paths.cs
@@ -3,17 +3,18 @@ using UnityEngine;
 
 public static class Paths
 {
-    public static string MainPath => Application.persistentDataPath;
+    public static string Persistent => Application.persistentDataPath;
+    public static string Streaming => Application.streamingAssetsPath;
 
-    public static string PlayerPresetsPath => Path.Combine(MainPath, "PlayerPresets");
+    public static string PlayerPresetsPath => Path.Combine(Persistent, "PlayerPresets");
     public static string PlayerPresetSpritesPath => Path.Combine(PlayerPresetsPath, "PlayerPresetSprites");
     public static string PlayerPresetDataPath => Path.Combine(PlayerPresetsPath, "PlayerPresetData");
 
-    public static string DevPresetsPath => Path.Combine(MainPath, "DevPresets");
+    public static string DevPresetsPath => Path.Combine(Streaming, "DevPresets");
     public static string DevPresetSpritesPath => Path.Combine(DevPresetsPath, "DevPresetSprites");
     public static string DevPresetDataPath => Path.Combine(DevPresetsPath, "DevPresetData");
 
-    public static string PlayerSavePath(string saveName) => Path.Combine(MainPath, saveName);
+    public static string PlayerSavePath(string saveName) => Path.Combine(Persistent, saveName);
 
     public static string ShipBuildsPath(string saveName) => Path.Combine(PlayerSavePath(saveName), "ShipBuilds");
 
